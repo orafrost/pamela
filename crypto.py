6b9e8c931a6bc522d93ff924661e538c15772e2b
orafrost <guillaume@MacBook-Pro-de-verrier.local>
2017-11-22 16:17:41 +0100

    pipe

--- crypto.py
+++ crypto.py
@@ -9,20 +9,20 @@ class crypto():
         self.user = user
 
     def creatVolume(self):
-        p  = Popen(["fallocate", "-l", "10M", "/home/" + self.user + '/' + self.user + "file", stdout=PIPE,stdin=PIPE, stderr=STDOUT])
-        p = Popen(["cryptsetup", "-y", "luksFormat", "/home/" + self.user + '/' +self.user + "file", stdin=PIPE, stderr=STDOUT])
+        p  = Popen(["fallocate", "-l", "10M", "/home/" + self.user + '/' + self.user + "file"], stdin=PIPE, stderr=STDOUT)
+        p = Popen(["cryptsetup", "-y", "luksFormat", "/home/" + self.user + '/' +self.user + "file"], stdin=PIPE, stderr=STDOUT)
         p.communicate(input=b'YES\n')
-        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume", stdin=PIPE, stderr=STDOUT])
-        p = Popen(["sudo", "mkfs.ext4", "/dev/mapper/" + self.user  + "volume", stdout=PIPE, stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume"], stdin=PIPE, stderr=STDOUT)
+        p = Popen(["sudo", "mkfs.ext4", "/dev/mapper/" + self.user  + "volume"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
 
     def openVolume(self):
-        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume", stdin=PIPE, stderr=STDOUT])
-        p = Popen(["sudo", "mount", "/dev/mapper/" + self.user + "volume", "/home/" + self.user + "test" + self.user + "file", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume"], stdin=PIPE, stderr=STDOUT)
+        p = Popen(["sudo", "mount", "/dev/mapper/" + self.user + "volume", "/home/" + self.user + "test" + self.user + "file"], stdin=PIPE, stderr=STDOUT)
 
     def closeVolume(self):
-        p = Popen(["sudo", "umount", "/home/" + self.user + "test" + self.user + "file", stdin=PIPE, stderr=STDOUT])
-        p = Popen(["sudo", "cryptsetup", "luksOpen", self.user + "volume", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "umount", "/home/" + self.user + "test" + self.user + "file"], stdin=PIPE, stderr=STDOUT)
+        p = Popen(["sudo", "cryptsetup", "luksOpen", self.user + "volume"], stdin=PIPE, stderr=STDOUT)
 
 if __name__ == "__main__":
     c = crypto("test", "guillaume")
