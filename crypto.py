c24bbcdb317de2f7cdd3618cb57fb6365fbe727f
orafrost <guillaume@MacBook-Pro-de-verrier.local>
2017-11-22 16:13:36 +0100

    pipe

--- crypto.py
+++ crypto.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import os
 
 
@@ -9,20 +9,21 @@ class crypto():
         self.user = user
 
     def creatVolume(self):
-        subprocess.call(["fallocate", "-l", "10M", "/home/" + self.user + '/' + self.user + "file"])
-        subprocess.call(["cryptsetup", "-y", "luksFormat", "/home/" + self.user + '/' +self.user + "file"])
-        subprocess.call(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume"])
-        subprocess.call(["sudo", "mkfs.ext4", "/dev/mapper/" + self.user  + "volume"])
+        p  = Popen(["fallocate", "-l", "10M", "/home/" + self.user + '/' + self.user + "file", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["cryptsetup", "-y", "luksFormat", "/home/" + self.user + '/' +self.user + "file", stdin=PIPE, stderr=STDOUT])
+        p.communicate(input=b'YES\n')
+        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "mkfs.ext4", "/dev/mapper/" + self.user  + "volume", stdout=PIPE, stdin=PIPE, stderr=STDOUT])
 
 
     def openVolume(self):
-        subprocess.call(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume"])
-        subprocess.call(["sudo", "mount", "/dev/mapper/" + self.user + "volume", "/home/" + self.user + "test" + self.user + "file"])
+        p = Popen(["sudo", "cryptsetup", "luksOpen", "/home/" + self.user + '/' + self.user + "file", self.user + "volume", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "mount", "/dev/mapper/" + self.user + "volume", "/home/" + self.user + "test" + self.user + "file", stdin=PIPE, stderr=STDOUT])
 
     def closeVolume(self):
-        subprocess.call(["sudo", "umount", "/home/" + self.user + "test" + self.user + "file"])
-        subprocess.call(["sudo", "cryptsetup", "luksOpen", self.user + "volume"])
+        p = Popen(["sudo", "umount", "/home/" + self.user + "test" + self.user + "file", stdin=PIPE, stderr=STDOUT])
+        p = Popen(["sudo", "cryptsetup", "luksOpen", self.user + "volume", stdin=PIPE, stderr=STDOUT])
 
 if __name__ == "__main__":
     c = crypto("test", "guillaume")
-    c.creatVolume()
+    c.closeVolume()
